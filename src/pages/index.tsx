89368f50252be584b541d57be55b591277a8f90d
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-28 04:46:54 +0530

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,22 @@ import { CanvasClient, CanvasInterface } from "@dscvr-one/canvas-client-sdk";
 import CanvasClientWrapper from "@/components/CanvasClientComponent";
 import SendTransaction from "@/components/SendTransaction";
 import Demo from "@/components/Demo";
+import TransactionForm from "@/components/TransactionForm";
+import { useCanvasClient } from "@/hooks/useCanvasClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function Home() {
+  const { client, user, content, isReady } = useCanvasClient();
+
   return (
       <main className="text-white">
         <h1>Home Page</h1>
         <CanvasClientWrapper />
         <Demo/>
-        <SendTransaction/>
+        {/* <SendTransaction/> */}
+        {client && <TransactionForm canvasClient={client}/>}
       </main>
   );
 }
